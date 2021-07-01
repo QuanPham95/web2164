9973d8b9b9d7c7065c1a6e1bc0f23fc597951b67
QuanPham95 <ptaqll5@gmail.com>
2021-07-01 17:14:07 +0700

    final

--- configs/db_config.js
+++ configs/db_config.js
@@ -1,6 +1,5 @@
 var DBconfigs = {
     clouddb: {
-        urldb : "mongodb+srv://dbAdmin:c18x7b42DnHs9qIB@cluster0.j4a1m.mongodb.net/ATN_SHOP?retryWrites=true&w=majority",
         dbname : "ATN_SHOP",
         dbusername : "dbAdmin",
         dbpassword : "c18x7b42DnHs9qIB",
